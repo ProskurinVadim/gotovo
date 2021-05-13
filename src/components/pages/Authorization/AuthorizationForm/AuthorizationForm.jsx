b0ea87293b1911d6446fe2e900942fd28ff84dcf
ProskurinVadim <proskurin.vadim@gmail.com>
2021-05-13 15:06:02 +0300

    Start adding connection

--- src/components/pages/Authorization/AuthorizationForm/AuthorizationForm.jsx
+++ src/components/pages/Authorization/AuthorizationForm/AuthorizationForm.jsx
@@ -16,7 +16,7 @@ const AuthorizationForm = ({loading}) => {
     const ButtonOnclick = useCallback(() => {
         emailValidation(emailValue);
         passwordValidation(passwordValue);
-        dispatch(authLogin("admin@gmail.com","Zhjckfd1945#" ));
+        dispatch(authLogin(emailValue,passwordValue));
        // if (!emailError && !passwordError) dispatch(authLogin("wow1996strap@gmail.com","Zhjckfd1945#" ))
     },[emailValue,passwordValue]);
 
